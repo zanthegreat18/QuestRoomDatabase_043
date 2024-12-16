41eee8c60bab07da7ff4717d49336db6b21865be
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-16 16:02:17 +0700

    penambahan fungsi pada interface repositor untuk fungsi update delete

--- app/src/main/java/com/tugas/data/data/repositori/RepositoriMhs.kt
+++ app/src/main/java/com/tugas/data/data/repositori/RepositoriMhs.kt
@@ -5,12 +5,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
-
     suspend fun deleteMhs(mahasiswa: Mahasiswa)
-
     suspend fun updateMhs(mahasiswa: Mahasiswa)
-
     fun getAllMhs(): Flow<List<Mahasiswa>>
-
     fun getMhs(nim: String): Flow<Mahasiswa>
 }
