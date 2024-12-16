cc1e7b1dd7786dc1e118dac028d59af90683e396
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-16 15:44:34 +0700

    penambahan File Detail Dan Home Dan Update

---
+++ app/src/main/java/com/tugas/data/ui/viewmodel/DetailNhsViewModel.kt
@@ -0,0 +1,89 @@
+package com.tugas.data.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pam_pert9.data.entity.Mahasiswa
+import com.example.pam_pert9.repository.RepositoryMhs
+import com.example.pam_pert9.ui.navigation.DestinasiDetail
+import com.tugas.data.data.entity.Mahasiswa
+import com.tugas.data.data.repositori.RepositoriMhs
+import com.tugas.data.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoriMhs,
+) : ViewModel(){
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState (
+                isLoading = true,
+            ),
+        )
+
+    fun deteleMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
+}
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+// store data from entity to ui
+fun Mahasiswa.toDetailUiEvent() : MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jeniskelamin = jeniskelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
--- app/src/main/java/com/tugas/data/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/tugas/data/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,2 +1,50 @@
 package com.tugas.data.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tugas.data.data.entity.Mahasiswa
+import com.tugas.data.data.repositori.RepositoriMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+
+
+data class HomeUIState (
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
+class HomeMhsViewModel(
+    private val repositoriMhs: RepositoriMhs
+) : ViewModel(){
+
+    val homeUiState: StateFlow<HomeUIState> = repositoriMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUIState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUIState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUIState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .statein(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUIState(
+                isLoading = true)
+        )
+}
