af6ef77fe15396b054e5690aa649ef926ddf120e
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-12-16 15:23:02 +0700

    penambahan untuk alamat destinasi

--- app/src/main/java/com/tugas/data/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/tugas/data/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,20 @@ package com.tugas.data.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route: String = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route: String = "detail"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route: String = "update"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+
+}
